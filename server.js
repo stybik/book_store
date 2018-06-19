702e444533c66e03a1befcdc7a5454d17e825b7a
stybik <bikrocksbiswas@gmail.com>
2018-06-20 02:20:42 +0530

    add projects page

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ app.get('/about',(req, res)=>
   });
 });
 
+app.get('/projects', (req, res) =>
+{
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
 //bad 
 app.get('/bad', (req, res) =>
 {
